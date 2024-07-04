c6560a56702b25928df5ec8fd3e74e98a8ba5b22
Harry Duong <harryoduong2405@gmail.com>
2024-07-04 13:52:58 +0700

    Finished code, need to fix css to look nicer

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import "./styles.css";
+import "./App.css";
+import TicketForm from "./components/TicketForm";
+import {useReducer} from 'react';
+import ticketReducer from "./reducers/ticketReducer";
+import TicketList from "./components/TicketList";
+import {sortTickets} from "./utilities/sortingUtilities";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+
+    const initialState = {tickets: [], editingTicket: null, sortPreferences: "High to Low"};
+    const [state, dispatch] = useReducer(ticketReducer, initialState);
+    const sortedTickets = sortTickets(state.tickets, state.sortPreferences)
+
+    return (
+        <div className="App">
+            <div className='container'>
+                <h1>Bug Blaster</h1>
+            </div>
+            <TicketForm
+                dispatch={dispatch}
+                editingTicket = {state.editingTicket}
+            ></TicketForm>
+
+            {state.tickets.length > 0 &&
+             <div className='results'>
+                 <h2>All Tickets</h2>
+
+                 <select value={state.sortPreferences}
+                         onChange={e =>
+                             dispatch({type:"SET_SORTING", payload: e.target.value})}>
+                     <option value="High to Low">High to Low</option>
+                     <option value="Low to High">Low to High</option>
+                 </select>
+                 <TicketList
+                     tickets={sortedTickets}
+                     dispatch={dispatch}>
+                 </TicketList>
+             </div>
+            }
+        </div>
+    );
 }
-
-export default App;
